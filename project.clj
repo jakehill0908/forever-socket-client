d507a76e7fab3afbbba3d2b75bcffe7ef83f239c
Jake Hill <jakehill0908@gmail.com>
2018-11-07 22:16:47 -0700

    Added async

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject forever-socket-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "Socket clients that attempt to stay connected to server indefinitlely"
+  :url "https://github.com/jakehill0908/forever-socket-client"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]])

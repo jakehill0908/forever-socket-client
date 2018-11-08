a79f00b4f31a235411fd099ca9c65e9af507e6ba
Jake Hill <jakehill0908@gmail.com>
2018-11-07 22:37:38 -0700

    Implemented read and write into socket function

--- src/forever_socket_client/core.clj
+++ src/forever_socket_client/core.clj
@@ -1,43 +1,44 @@
 (ns forever-socket-client.core
-  (:import [java.net Socket InetAddress SocketException]
-           [java.io ByteArrayInputStream ByteArrayOutputStream])
+  (:import [java.net Socket InetAddress SocketException])
   (:require [clojure.core.async :refer [put! chan <! >! <!! go-loop]]))
+
+(declare start-socket-read!)
+(declare socket-write)
 
 (defn socket
   "Instantiate a java socket with SO_KEEPALIVE"
-  [^String host ^Integer port]
+  [^String host ^Integer port ^Integer read-buffer-size]
   (let [sock (Socket. (InetAddress/getByName host) port)]
     (.setKeepAlive sock true)
-    sock))
+    {:read-channel (start-socket-read! (.getInputStream sock) read-buffer-size)
+     :write (socket-write (.getOutputStream sock))
+     :socket sock}))
+
+(defn socket-write
+  "Closure for simpler write to socket"
+  [output-stream]
+  (let [send-to-socket (fn [data]
+                         (.write output-stream data 0 (count data)))]
+    send-to-socket))
 
 (defn start-socket-read!
-  [input-stream buffer-size stream-closed-cb]
+  "Setup a receive channel and shuffle data to it on receive"
+  [input-stream buffer-size]
   (let [read-channel (chan)
         start-read (fn [raw-buffer]
                      (try
                        (let [count (.read input-stream raw-buffer)]
-                         (take count raw-buffer))
+                         (byte-array (take count raw-buffer)))
                        (catch SocketException e
-                         (do
-                           (stream-closed-cb)
-                           :closed))))]
+                           :closed)))]
     (go-loop []
       (let [data (start-read (byte-array buffer-size))]
         (if (= data :closed)
-          data
+          nil
           (do
             (put! read-channel data)
             (recur)))))
     read-channel))
-
-(defn structure
-  "Prototyping"
-  [^Socket socket]
-  (let [ostream (.getOutputStream socket)
-        istream (.getInputStream socket)]
-    {:ostream ostream
-     :istream istream
-     :socket socket}))
 
 (defn str-to-bytes
   "Convert things to bytes"

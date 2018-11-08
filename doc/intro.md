8f94ec17337f900314e67d85c2870f21a2780399
Jake Hill <jakehill0908@gmail.com>
2018-11-07 19:09:41 -0700

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to forever-socket-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/forever_socket_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns forever-socket-client.core-test
+  (:require [clojure.test :refer :all]
+            [forever-socket-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
